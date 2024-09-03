c4f4d6359aad98d8b693476094aa19dc8ec794d8
gavinha <gui.mgava@gmail.com>
2024-09-02 22:42:53 -0300

    Update

---
+++ key.sh
@@ -0,0 +1,36 @@
+#!/bin/usr/env bash
+
+
+
+# Insert Pendrive
+echo "insert the "ARCH" pendrive and type 'C' to continue"
+read -r resposta
+
+if [[ "$resposta" =~ ^[Cc]$ ]]; then
+    echo "Continuing..."
+    # Coloque o restante do código aqui
+else
+    echo "Exiting..."
+    exit 1
+fi
+
+# Configure Git
+
+git config --global user.name "gavinha"
+git config --global user.email "gui.mgava@gmail.com"
+
+# Configure SSH Key
+cp -r /run/media/gavinha/ARCH/.ssh ~
+chmod 700 ~/.ssh/id_ed25519
+chmod 644 ~/.ssh/id_ed25519.pub
+
+# Clone Memory-Card
+git clone "git@github.com:Gavinhas/memory-card.git"
+
+# Install ZSH
+sudo pacman -S zsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cd ~/memory-card
+
+./script/script.sh
