fd1b594b8bb39791636f4f2dc2afef5439044bbe
gavinha <gui.mgava@gmail.com>
2025-04-25 12:31:17 -0300

    Update key.sh

--- key.sh
+++ key.sh
@@ -4,13 +4,12 @@
 echo "insert the "ARCH" pendrive and type 'C' to continue"
 read -r resposta
 
-if [[ "$resposta" =~ ^[Cc]$ ]]; then
-    echo "Continuing..."
-    # Coloque o restante do código aqui
-else
+if [[ "$resposta" != ^[Cc]$ ]]; then
     echo "Exiting..."
-    exit 1
+    exit 1    
 fi
+
+echo "Continuing..."
 
 # Configure Git
 
@@ -18,9 +17,16 @@ git config --global user.name "gavinha"
 git config --global user.email "gui.mgava@gmail.com"
 
 # Configure SSH Key
-cp -r /run/media/gavinha/ARCH/.ssh ~
+
+mkdir -p ~/.ssh
+
+cp /run/media/gavinha/Ventoy/.ssh/id_ed25519 ~
+cp /run/media/gavinha/Ventoy/.ssh/id_ed25519.pub ~
+
 chmod 700 ~/.ssh/id_ed25519
 chmod 644 ~/.ssh/id_ed25519.pub
+
+echo "SSH keys copied and permissions set."
 
 # Clone Memory-Card
 git clone "git@github.com:Gavinhas/memory-card.git"
@@ -28,5 +34,10 @@ git clone "git@github.com:Gavinhas/memory-card.git"
 # Install ZSH
 sudo pacman -S zsh
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sed -i 's/robbyrussell/amuse/g' ~/.zshrc
+
+# Run Memory-Card
+
+chmod +x memory-card/script/arch.sh
+bash memory-card/script/arch.sh
+
 exit
