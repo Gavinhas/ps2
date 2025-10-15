a5cb69164833303f5c8f04fa115f9d84477fc43a
gavinha <gui.mgava@gmail.com>
2025-10-15 10:17:31 -0300

    Updates 15/10/2025

--- key.sh
+++ key.sh
@@ -1,10 +1,10 @@
-#!/bin/env/usr bash
+#!/usr/bin/env bash
 
 # Insert Pendrive
-echo "insert the "ARCH" pendrive and type 'C' to continue"
-read -r resposta
+echo -e "\n>>> Insert the 'ARCH' pendrive and type 'C' to continue <<<\n"
+read -r RESPOSTA
 
-if [[ "$resposta" != ^[Cc]$ ]]; then
+if [[ "$RESPOSTA" != "C" && "$RESPOSTA" != "c" ]]; then
     echo "Exiting..."
     exit 1    
 fi
@@ -18,26 +18,58 @@ git config --global user.email "gui.mgava@gmail.com"
 
 # Configure SSH Key
 
-mkdir -p ~/.ssh
+echo "Configure SSH? | [Y] Yes, [N] No"
+read -r CONFIGURESSH
 
-cp /run/media/gavinha/Ventoy/.ssh/id_ed25519 ~
-cp /run/media/gavinha/Ventoy/.ssh/id_ed25519.pub ~
+if [[ "$CONFIGURESSH" =~ ^[Yy]$ ]]; then
 
-chmod 700 ~/.ssh/id_ed25519
-chmod 644 ~/.ssh/id_ed25519.pub
+    PENDRIVE_PATH="/run/media/$USER/Ventoy/.ssh"
 
-echo "SSH keys copied and permissions set."
+    if [[ -f "$PENDRIVE_PATH/id_ed25519" && -f "$PENDRIVE_PATH/id_ed25519.pub" ]]; then
 
-# Clone Memory-Card
-git clone "git@github.com:Gavinhas/memory-card.git"
+        mkdir -p ~/.ssh
+
+        cp "$PENDRIVE_PATH/id_ed25519" ~/.ssh/
+        cp "$PENDRIVE_PATH/id_ed25519.pub" ~/.ssh/
+
+        chmod 600 ~/.ssh/id_ed25519
+        chmod 644 ~/.ssh/id_ed25519.pub
+
+        echo "SSH keys copied and permissions set."
+    else
+        echo "Error: SSH Not found on the Pendrive"
+        exit 1
+    fi
+
+fi
+
+
 
 # Install ZSH
-sudo pacman -S zsh
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Install ZSH? | [Y] Yes, [N] No"
+read -r CONFIGUREZSH
+
+if [[ "$CONFIGUREZSH" =~ ^[Yy]$ ]]; then
+    sudo pacman -S zsh --needed --noconfirm
+    sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+# Clone Memory-Card
+
+echo "Clone Memory-Card? | [Y] Yes, [N] No"
+read -r CMEMORYCARD
+
+if [[ "$CMEMORYCARD" =~ ^[Yy]$ ]]; then
+    git clone "git@github.com:Gavinhas/memory-card.git"
+fi
 
 # Run Memory-Card
 
-chmod +x memory-card/script/arch.sh
-bash memory-card/script/arch.sh
+echo "Run Memory-Card? | [Y] Yes, [N] No"
+read -r RMEMORYCARD
 
-exit
+if [[ "$RMEMORYCARD" =~ ^[Yy]$ ]]; then
+    chmod +x memory-card/script/arch.sh
+    sudo bash memory-card/script/arch.sh
+fi
